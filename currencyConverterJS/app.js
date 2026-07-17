215599ca01a3190708e3708edaf976ec96225f62
Mirajul Mollah <mirajulmollah2000@gmail.com>
2026-07-17 14:23:21 +0530

    Initial commit

---
+++ currencyConverterJS/app.js
@@ -0,0 +1,39 @@
+const URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json";
+
+
+const dropdowns=document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+
+for(let select of dropdowns){
+    for(let currCode in countryList){
+        console.log(currCode)
+        let newOption=document.createElement("option")
+        newOption.innerText=currCode;
+        newOption.value=currCode;
+        if(select.name==="from"&& currCode==="USD"){
+            newOption.selected="selected";
+        }
+        else if (select.name==="to"&& currCode==="INR"){
+            newOption.selected="selected";
+        }
+        select.append(newOption)
+    }
+    select.addEventListener("change",(evt)=>{
+        updateFlag(evt.target)
+    })
+}
+function updateFlag(element) {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  
+});
